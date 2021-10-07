4960123f87e793e7d86ff3ab07bb42b875ba980e
Viraj Lakshitha Bandara <virajlakshithabandara@gmail.com>
2021-10-07 07:52:45 +0530

    Project Initialized

---
+++ src/main/java/lk/devstory/devstory/DevstoryServicesApplication.java
@@ -0,0 +1,13 @@
+package lk.devstory.devstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstoryServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstoryServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/devstory/devstory/DevstoryServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.devstory.devstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstoryServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
